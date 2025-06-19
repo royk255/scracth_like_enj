ae147309c486374df8d3c3e9c42286e84c2f584d
royk255 <roykam2008@gmail.com>
2025-06-19 15:28:58 +0300

    move to classes and add charater

--- block_classes.py
+++ block_classes.py
@@ -1,33 +1,30 @@
 import pygame
 import sys
+import os
+import defines
 
 
-FULL_SCREEN_WIDTH = 1920
-FULL_SCREEN_HEIGHT = 1080
-HALF_SCREEN_WIDTH =  FULL_SCREEN_WIDTH // 2
-HALF_SCREEN_HEIGHT = FULL_SCREEN_HEIGHT // 2
 
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
-screen = pygame.display.set_mode((FULL_SCREEN_WIDTH,  FULL_SCREEN_HEIGHT), pygame.RESIZABLE)
+screen = pygame.display.set_mode((defines.FULL_SCREEN_WIDTH,  defines.FULL_SCREEN_HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Snapping Blocks")
 clock = pygame.time.Clock()
 
-# Colors
-WHITE = (255, 255, 255)
-BLUE = (100, 149, 237)
 
 # Font
 pygame.font.init()
 FONT = pygame.font.SysFont("Arial", 24)
 
 blocks = []
+list_of_characters = []
 
 line = pygame.Rect(1600, 0, 20, 1080)  # Vertical line on the right
 SAFE_BLOCKS_LOC = line.x + 50 
 
 
 class Block:
-    def __init__(self, x, y, text, type, size, color=BLUE):
+    def __init__(self, x, y, text, type, size, color=defines.BLUE):
         self.rect = pygame.Rect(x, y, size[0], size[1])
         self.text = text
         self.dragging = False
@@ -38,7 +35,7 @@ class Block:
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect, border_radius=10)
-        label = FONT.render(self.text, True, WHITE)
+        label = FONT.render(self.text, True, defines.WHITE)
         surface.blit(label, (self.rect.x + 10, self.rect.y + 15))
 
     def handle_event(self, event, blocks):
@@ -50,7 +47,7 @@ class Block:
                     self.offset_x = self.rect.x - mouse_x
                     self.offset_y = self.rect.y - mouse_y
                 else:
-                    blocks.append(Block(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, self.text))
+                    blocks.append(Block(defines.HALF_SCREEN_WIDTH, defines.HALF_SCREEN_HEIGHT, self.text, self.type, (150, 50), self.color))
 
         elif event.type == pygame.MOUSEBUTTONUP:
             if self.dragging:
@@ -71,42 +68,12 @@ class Block:
                 if dx < 20 and dy < 20:
                     self.rect.x = block.rect.x
                     self.rect.y = block.rect.y + block.rect.height
-
-    def logic(self):
-        pass
     
-
-class BlockType(Block):
-    def __init__(self, x, y, text, type, size, color=BLUE):
-        super().__init__(x, y, text, type, size, color)
-        self.type = type
-
-    def logic(self):
-        # Implement specific logic for different block types if needed
-        pass
-
-
-
-class character(BlockType):
-    def __init__(self, x, y, text, size, color=BLUE):
-        super().__init__(x, y, text, "character", size, color)
-
-    def logic(self):
-        # Implement specific logic for character block
-        pass
-
-
-
-class move_character(BlockType):
-    def __init__(self, x, y, text, size, color=BLUE):
-        super().__init__(x, y, text, "move_character", size, color)
-        self.param1 = 10
-
     def get_input(self):
-        screen = pygame.display.set_mode((640, 480))
+        screen_input = pygame.display.set_mode((300, 200), pygame.RESIZABLE)
         font = pygame.font.Font(None, 32)
         clock = pygame.time.Clock()
-        input_box = pygame.Rect(100, 100, 140, 32)
+        input_box = pygame.Rect(50, 100, 140, 32)
         color_inactive = pygame.Color('lightskyblue3')
         color_active = pygame.Color('dodgerblue2')
         color = color_inactive
@@ -119,7 +86,6 @@ class move_character(BlockType):
                 if event.type == pygame.QUIT:
                     done = True
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # If the user clicked on the input_box rect.
                     if input_box.collidepoint(event.pos):
                         active = not active
                     else:
@@ -129,26 +95,122 @@ class move_character(BlockType):
                     if active:
                         if event.key == pygame.K_RETURN:
                             print(text)
-                            self.param1 = text  # Store the input value in the block's parameter
+                            self.param1 = text
                             done = True
                         elif event.key == pygame.K_BACKSPACE:
                             text = text[:-1]
                         else:
                             text += event.unicode
 
-            screen.fill((30, 30, 30))
+            screen_input.fill(defines.WHITE)
+            # Render and blit the label so it's visible
+            label = FONT.render("Enter a value:", True, defines.BLACK)
+            screen_input.blit(label, (50, 50))
             # Render the current text.
             txt_surface = font.render(text, True, color)
-            # Resize the box if the text is too long.
             width = max(200, txt_surface.get_width()+10)
             input_box.w = width
-            # Blit the text.
-            screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
-            # Blit the input_box rect.
-            pygame.draw.rect(screen, color, input_box, 2)
+            screen_input.blit(txt_surface, (input_box.x+5, input_box.y+5))
+            pygame.draw.rect(screen_input, color, input_box, 2)
 
             pygame.display.flip()
             clock.tick(30)
+
+    def logic(self):
+        pass
+    
+
+class BlockType(Block):
+    def __init__(self, x, y, text, type, size, color=defines.BLUE):
+        super().__init__(x, y, text, type, size, color) 
+        self.type = type
+
+    def logic(self):
+        # Implement specific logic for different block types if needed
+        pass
+
+
+class Character:
+    def __init__(self, image_path, x=0, y=0, rotation=0, scale=1.0, facing_right=True):
+        self.image_original = pygame.image.load(image_path).convert_alpha()
+        self.character_name = os.path.splitext(os.path.basename(image_path))[0]
+        self.scale = scale
+        if scale != 1.0:
+            w, h = self.image_original.get_size()
+            self.image_original = pygame.transform.smoothscale(
+                self.image_original, (int(w * scale), int(h * scale))
+            )
+        self.facing_right = facing_right
+        self.image = self.image_original
+        self.x = x
+        self.y = y
+        self.rotation = rotation
+        self.update_rect()
+        self.cmds = []
+
+    def update_rect(self):
+        img = pygame.transform.rotate(self.image_original, -self.rotation)
+        if not self.facing_right:
+            img = pygame.transform.flip(img, True, False)
+        self.image = img
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        self.update_rect()
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.update_rect()
+
+    def set_rotation(self, angle):
+        self.rotation = angle % 360
+        self.update_rect()
+
+    def flip(self):
+        self.facing_right = not self.facing_right
+        self.update_rect()
+
+    def set_direction(self, right=True):
+        self.facing_right = right
+        self.update_rect()
+
+    def get_direction(self):
+        return "right" if self.facing_right else "left"
+
+    def get_hitbox(self):
+        return self.rect
+
+    def collides_with(self, other_rect):
+        return self.rect.colliderect(other_rect)
+
+list_of_characters.append(Character("C:\\Data\\roy\\my_projects\\game_enj_sc_like\\scracth_like_enj\\defult_character.jpg", 100, 100))
+
+
+class move_character(BlockType):
+    def __init__(self, x, y, size, character, color=defines.BLUE):
+        super().__init__(x, y, "move_character", "Motion", size, color)
+        self.character = character
+        self.param1 = 10
+        self.has_param = 1
+
+    def logic(self):
+        self.get_input()
+        print(self.param1)
+        pass
+
+class turn_character(BlockType):
+    def __init__(self, x, y, size, character, color=defines.BLUE):
+        super().__init__(x, y, "turn_character", "Motion", size, color)
+        self.character = character
+        self.param1 = 10
+        self.has_param = 1
+
 
     def logic(self):
         self.get_input()
@@ -156,8 +218,53 @@ class move_character(BlockType):
         pass
 
 
+blocks_text = [
+    "Move 10 Steps",
+    "Jump",
+    "Turn Left",
+    "Turn Right",
+    "Move Backward"
+]
+def add_blocks():
+    for i in range(len(blocks_text)):
+        x = SAFE_BLOCKS_LOC 
+        y = 20 + (i * 60)  
+        if blocks_text[i] == "Move 10 Steps":
+            blocks.append(move_character(x, y, (150, 50), list_of_characters[0]))
+        elif blocks_text[i] == "Turn Left":
+            blocks.append(turn_character(x, y, (150, 50), list_of_characters[0]))
+        else:
+            blocks.append(BlockType(x, y, blocks_text[i], "Motion", (150, 50)))
+
+def main():
+    running = True
+    add_blocks()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            for block in blocks:
+                block.handle_event(event, blocks)
+
+        screen.fill(defines.WHITE)
+        for block in blocks:
+            block.draw(screen)
+
+        # Draw the vertical line
+        pygame.draw.rect(screen, defines.BLACK, line)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+    sys.exit()
+"""
 pygame.init()
-bla =  move_character(100, 100, "Move Character", (150, 50))
+bla =  move_character(100, 100, (150, 50), list_of_characters[0])
 bla.logic()
 pygame.quit()
 sys.exit()
+"""
+if __name__ == "__main__":
+    main()
